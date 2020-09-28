fb51a7305b472b424808d67dabbc922d290365a3
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-29 01:05:06 +0300

    scroll mechanics

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:scale3c_demo_app/scale3c_demo_app.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar(), bottomNavigationBar: CustomAppNavigation());
+        appBar: CustomAppBar(),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              height: 200.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 160.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 160.0,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 200.0,
+              color: Colors.red,
+            ),
+            Container(
+              height: 200.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 200.0,
+              color: Colors.black45,
+            ),
+            Container(
+              height: 200.0,
+              color: Colors.orange,
+            ),
+            Container(
+              height: 200.0,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+        bottomNavigationBar: CustomAppNavigation());
   }
 }
