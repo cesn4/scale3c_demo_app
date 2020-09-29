c83dce3ef276b50a7767bfc14fc548e1693fa065
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-29 13:10:46 +0300

    Update home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:scale3c_demo_app/mocks/images_list.dart';
 import 'package:scale3c_demo_app/scale3c_demo_app.dart';
 
 class HomeView extends StatelessWidget {
