d9318253c3c01bf0ead91a8db701fe7530887cbd
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-29 15:30:16 +0300

    screens folder

--- lib/scale3c_demo_app.dart
+++ lib/scale3c_demo_app.dart
@@ -3,3 +3,4 @@ export './theme/index.dart';
 export './views/index.dart';
 export './mocks/index.dart';
 export './constants/index.dart';
+export './screens/index.dart';
--- lib/views/index.dart
+++ lib/views/index.dart
@@ -1,5 +1,3 @@
 export './home_view.dart';
-export './main_screen.dart';
 export './items_view.dart';
 export './cards_view.dart';
-export './add_screen.dart';
